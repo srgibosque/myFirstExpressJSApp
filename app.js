a58b01b66505863ca276808787df0e786f53317c
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-05 20:40:00 +0200

    Using middlewares and sending responses

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+
+const app = express();
+
+// use() adds a new middleware function 
+app.use((req, res, next) => {
+  console.log('Middleware');
+  //Thanks to next travels to the middleware below
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  //sends any response
+  res.send('<h1>Hello</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
