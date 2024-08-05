3d1e37c64a33d2e02afc63c9f75120ebdc59443e
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-05 21:04:16 +0200

    Filtering request types

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// use() adds a new middleware function 
-app.use((req, res, next) => {
-  console.log('Middleware');
-  //Thanks to next travels to the middleware below
-  next();
+//It parses the requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware');
+//same as app.use() but only trigger for post requests.
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+
+  res.redirect('/');
+});
+
+// The default response middleware
+app.use('/', (req, res, next) => {
   //sends any response
-  res.send('<h1>Hello</h1>');
+  res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
