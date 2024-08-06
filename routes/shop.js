0f7a22570e32a0b256ee0c0a09a4bf9bcb932474
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-06 20:15:26 +0200

    added file with root directory

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // The default response middleware
 router.get('/', (req, res, next) => {
   //sends any response
-  res.sendFile(path.join(__dirname,'../', 'views', 'shop.html')); 
+  res.sendFile(path.join(__dirname,'..', 'views', 'shop.html')); 
 });
 
 module.exports = router;
