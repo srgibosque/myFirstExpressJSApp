06eb029671028a5f1def3d694f7c9ef009efe133
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-06 21:06:47 +0200

    serving external css files staticlly

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const shopRoutes = require('./routes/shop');
 
 //It parses the requests
 app.use(bodyParser.urlencoded({ extended: false }));
+//loads static files like css
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
