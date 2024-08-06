d7934e864639a7b158b497ef928d64a0f8951c3f
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-06 20:05:26 +0200

    Serving HTML files

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 //same as app.use() but only trigger for post requests.
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
 
   res.redirect('/');
--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +6,7 @@ const router = express.Router();
 // The default response middleware
 router.get('/', (req, res, next) => {
   //sends any response
-  res.send('<h1>Hello from express</h1>');
+  res.sendFile(path.join(__dirname,'../', 'views', 'shop.html')); 
 });
 
 module.exports = router;
