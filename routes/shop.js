b2d462cc7353e5562bd75edce628d9bb619e3c9c
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-06 20:59:18 +0200

    inline styles

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // The default response middleware
 router.get('/', (req, res, next) => {
   //sends any response
-  res.sendFile(path.join(__dirname,'..', 'views', 'shop.html')); 
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
 });
 
 module.exports = router;
