3013898c1f1439063bed89dbde86261868f262b3
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-08-08 20:07:22 +0200

    passing dynamic data

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,10 @@ const adminData = require('./admin');
 // The default response middleware
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
+  const products = adminData.products;
   //sends a pug file, we only write shop cause the root folder is already declared in app.js
-  res.render('shop');
+  // we pass data to the template
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
